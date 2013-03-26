4d31c09a877a76d8f327cf71eb1be81e8d104553
Florian Maier <contact@marsmenschen.com>
2013-03-26 09:35:43 +0100

    initial german translation

---
+++ Libraries/Labels/de_SM-secrets.php
@@ -0,0 +1,82 @@
+<?php
+/**
+* Libellé générique en Anglais.
+*
+* @warning Ce fichier doit impérativement être au format UTF-8 pour une gestion cohérente des caractères accentués.
+*
+* @copyright LGPL License 3.0 http://www.gnu.org/copyleft/lesser.html
+* @author Pierre-Luc MARY, Florian Maier (contact@marsmenschen.com)
+* @date 2013-03
+* @version 1.2
+*/
+
+	$L_Title = 'Secrets management';
+	
+	$L_List_Groups = 'Liste der Gruppen';
+	$L_List_Secrets = 'Liste der Secrets';
+	$L_List_Entities = 'Liste der Instanzen';
+	$L_Search_Secrets = 'Secrets durchsuchen';
+	
+	$L_Group_Create = 'Gruppe Secrets erstellen';
+	$L_Group_Delete = 'Gruppe Secrets löschen';
+	$L_Group_Modify = 'Gruppe Secrets bearbeiten';
+	
+	$L_Secret_Create = 'Secret erstellen';
+	$L_Secret_Delete = 'Secret löschen';
+	$L_Secret_Modify = 'Secret bearbeiten';
+	$L_Secret_View = 'Secret ansehen';
+	$L_Password_View = 'Passwort';
+	
+	$L_Group = 'Gruppe Secrets';
+	$L_Groups = 'Gruppen Secrets';
+	$L_Type = 'Typ';
+	$L_Environment = 'Umgebung';
+	$L_Application = 'Anwendung';
+	$L_Secret = 'Secret';
+	$L_Host = 'Host';
+	$L_User = 'User';
+	$L_Password = 'Passwort';
+	$L_Creation_Date = 'Erstellungsdatum';
+	$L_Modification_Date = 'Bearbeitungsdatum';
+	
+	$L_Group_Created = 'Gruppe Secrets erstellt' ;
+	$L_Group_Modified = 'Gruppe Secrets geändert' ;
+	$L_Group_Deleted = 'Gruppe Secrets gelöscht' ;
+	
+	$L_ERR_CREA_Group = 'Fehler beim Erstellen einer Gruppe von Secrets';
+	$L_ERR_MODI_Group = 'Fehler beim Bearbeiten einer Gruppe von Secrets';
+	$L_ERR_DELE_Group = 'Fehler beim Löschen einer Gruppe von Secrets';
+	$L_ERR_DUPL_Group = '"Label" einer Gruppe von Secrets existiert bereits';
+	
+	$L_Secret_Created = 'Secret erstellt' ;
+	$L_Secret_Modified = 'Secret geändert' ;
+	$L_Secret_Deleted = 'Secret gelöscht' ;
+	
+	$L_Profiles_Clean = 'Lösche Profile vor Neuerstellung';
+	
+	$L_ERR_CREA_Secret = 'Fehler beim Erstellen des Secret';
+	$L_ERR_MODI_Secret = 'Fehler beim Bearbeiten des Secret';
+	$L_ERR_DELE_Secret = 'Fehler beim Löschen des Secret';
+	$L_ERR_DUPL_Secret = '"Secret" mit selben Eigenschaften existiert bereits';
+	
+	$L_Group_Profiles = 'Profile mit Gruppe von Secrets verbinden';
+	$L_Profile_Groups = 'Gruppe von Secrets mit Profilen verbinden';
+	$L_Group_Secrets = 'Secrets mit Gruppe Secrets verbinden';
+	$L_Groups_Management = 'Gruppes von Secrets verwalten';
+	$L_Profiles_Associate = 'Profile verbinden';
+	$L_Secrets_Associate = 'Secrets verbinden';
+	$L_Groups_Associate = 'Gruppe von Secrets verbinden';
+	$L_Secret_Management = 'Secrets verwalten';
+	$L_Association_Complited = 'Verbindung abgeschlossen';
+	
+	$L_No_Profile = 'Kein Profil';
+	$L_Profile_Not_Found = 'Profil nicht gefunden';
+	
+	$L_Generate = "Generate";
+	
+	$L_No_Good_Size = 'Länge reicht nicht aus (min)';
+	$L_Use_Lowercase = 'Kleinbuchstaben verwenden';
+	$L_Use_Uppercase = 'Großbuchstaben verwenden';
+	$L_Use_Number = 'Ziffer verwenden';
+	$L_Use_Special_Chars = 'Sonderzeichen verwenden';
+?>
---
+++ Libraries/Labels/de_labels_generic.php
@@ -0,0 +1,84 @@
+<?php
+/**
+* Libellé générique en Anglais.
+*
+* @warning Ce fichier doit impérativement être au format UTF-8 pour une gestion cohérente des caractères accentués.
+*
+* @copyright LGPL License 3.0 http://www.gnu.org/copyleft/lesser.html
+* @author Pierre-Luc MARY, Florian Maier (contact@marsmenschen.com)
+* @date 2013-03
+* @version 1.2
+*/
+
+	$L_Subtitle = "Password Gruppenverwaltung";
+	$L_Langue_en = "Englisch";
+	$L_Langue_de = "Deutsch";
+	$L_Langue_fr = "Französisch";
+	$L_No_Authorize = "Zugriff nicht erlaubt";
+	$L_Default = 'Standard';
+	$L_Actions = "Aktionen";
+	$L_Modify = "Bearbeiten";
+	$L_Create = "Erstellen";
+	$L_Delete = "Löschen";
+	$L_Cancel = "Abbrechen";
+	$L_Verify = "Überprüfen";
+	$L_Reset = "Zurücksetzen";
+	$L_Select = "Auswählen";
+	$L_Search = "Suchen";
+	$L_Updated = "Aktualisiert";
+	$L_Status = "Status";
+	$L_Enable = "Aktiviert";
+	$L_Disable = "Deaktiviert";
+	$L_Return = "Zurück";
+	$L_Alert = "Achtung";
+	$L_Name = "Name";
+	$L_Yes = "Ja";
+	$L_No = "Nein";
+	$L_Code = "Code";
+	$L_Label = "Label";
+	$L_Rights = "Berechtigungen";
+	$L_Profiles = "Profile";
+	$L_Profil = "Profile";
+	$L_Secrets = 'Passwörter';
+	$L_Comment = 'Kommentare';
+	$L_Dashboard = "Dashboard";
+	$L_Secrets_Management = "Passwörter verwalten";
+	$L_Users_Management = "Benutzer verwalten";
+	$L_Referentials_Management = "Repositories verwalten";
+	$L_Civilities_Management = "Civilities Management";
+	$L_Entities_Management = "Instanzen verwalten";
+	$L_Preferences_Management = "Einstellungen verwalten";
+	$L_Group_List = 'Gruppierte Liste';
+	$L_Detail_List = 'Detailierte Liste';
+	$L_Associate = "Verbunden";
+	$L_Invalid_Characters = 'Ungültige Zeichen';
+	$L_Invalid_Value = 'Ungültiger Wert';
+	$L_Close = "Schließen";
+	$L_View = "Betrachten";
+	$L_Deconnexion = "Abmelden";
+	$L_Changed_Password = "Passwort ändern";
+	$L_From = "Von";
+	$L_To = "An";
+	$L_Referential = "Referential";
+	$L_Referential_Management = "Referential Management";
+	$L_Types = 'Types';
+	$L_Environments = "Umgebungen";
+	$L_Historical = "Historie";
+	$L_Historical_Management = "Historie verwalten";
+	$L_Date = 'Datum';
+	$L_Identity = 'Identität';
+	$L_IP_Source = 'IP Zugriff';
+	$L_Message = 'Nachricht';
+	$L_Action = 'Aktion';
+	$L_Begin = 'Anfang';
+	$L_End = 'Ende';
+	$L_Next = 'vor';
+	$L_Previous = 'zurück';
+	$L_Purge = 'löschen';
+	$L_SecretServer = 'SecretServer';
+	$L_Load = 'Laden';
+	$L_Execute = 'Ausführen';
+
+	$FLAG_ERROR = 0;
+	$FLAG_SUCCESS = 1;
+?>
